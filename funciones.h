3e0717a9280014f23d4f34d9c14d8dde0d9bbddf
ErntDev <ernt26dev@gmail.com>
2015-07-05 18:47:55 -0500

    Agregué nuevo archivo funciones.h
    Hice un nuevo archivo llamado funciones.h el cual
    contendrá todas las funciones del programa. Le anexé
    las cuatro funciones del programa principal

---
+++ funciones.h
@@ -0,0 +1,4 @@
+void ingresarCoeficientes(double *M[], int );
+void mostrarSistemaEcuaciones(double *M[], int n);
+void gaussSimpleEtapa1(double *M[], int n);
+void gaussSimpleEtapa2(double *M[], double *X, int n);
